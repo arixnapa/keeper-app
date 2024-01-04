575b72053af63c55cda05ee1f510392c42ac19b9
Arianne Napa <arianne.napa@gmail.com>
2024-01-04 19:14:24 +0100

    Created CreateArea

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       <Note />
       <Footer />
     </div>
