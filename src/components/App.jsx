1690da593a99dc60d2aa19041ffb408c7e7f8355
Arianne Napa <arianne.napa@gmail.com>
2024-01-04 19:47:14 +0100

    Added note to an array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,16 @@ import Footer from "./Footer";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
